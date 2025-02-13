4d494db1f01c850f0bfa8865bc01b2f250d96326
NIMA <nima85sohrabi@gmail.com>
2025-02-13 16:33:41 +0330

    first commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='sticky top-0 mt-2 grid grid-cols-8 container mx-auto w-full border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
+        <div>
+            <h1 className=''></h1>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
